bae197816b5e4a8b661bf215d74ef2d14a433e40
Ashlyn Knox <ashlyn@lilyx.io>
2023-02-15 11:05:33 -0700

    index file

---
+++ index.js
@@ -0,0 +1,9 @@
+const _ = require("lodash");
+
+// capitalize this name
+const myName = "ashlyn";
+
+const capitalizedName = _.capitalize(myName);
+
+console.log(myName);
+console.log(capitalizedName);
